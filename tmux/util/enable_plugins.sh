b0e6fbaa0987021278a5ff9506bcf13a1be0c0fe
lambdalisue <lambdalisue@hashnote.net>
2019-10-12 02:55:58 +0900

    Simplify tmux.conf

--- tmux/util/enable_plugins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ ! -d ~/.tmux/plugins/tpm ]]; then
-  mkdir -p ~/.tmux/plugins
-  git clone --single-branch --depth 1 \
-    https://github.com/tmux-plugins/tpm \
-    ~/.tmux/plugins/tpm
-fi
-source $HOME/.tmux/plugins/tpm/tpm
