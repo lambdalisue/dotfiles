bdc0b1ad1eb0ba4261edd239e41a22e9f093cc98
lambdalisue <lambdalisue@hashnote.net>
2019-11-27 09:43:00 +0900

    Update

--- zsh/rc.d/90_functions.zsh
+++
@@ -1,8 +0,0 @@
-autoload -Uz brew_cask_upgrade
-autoload -Uz zsh_profile
-autoload -Uz zsh_profile_plain
-autoload -Uz zsh_cache_build
-autoload -Uz zsh_cache_clear
-autoload -Uz test_ecma48
-autoload -Uz test_256color
-autoload -Uz test_truecolor
