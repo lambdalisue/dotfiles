ed28b2e63f75ea895ce2437a653d781e9bb4c31e
lambdalisue <lambdalisue@hashnote.net>
2020-05-15 11:47:22 +0900

    Add fzf-kubectl

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -59,8 +59,7 @@ if type fzf &>/dev/null; then
   abindkey '^X^B' fzf-bindkey
   abindkey '^X^K' fzf-kill
   abindkey '^X^G' fzf-ghq
-  abindkey '^X^L' fzf-kubectl-logs
-  abindkey '^X^E' fzf-kubectl-exec
+  abindkey '^X^P' fzf-kubectl
 fi
 
 # xdg-open
