f7073249326d267c78ca3f05886cc34aed436bd2
Alisue <lambdalisue@gmail.com>
2024-09-25 04:08:02 +0900

    Add PowerShell Profile

---
+++ .config/Profile.ps1
@@ -0,0 +1,17 @@
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Chord 'Ctrl+x,Ctrl+g' -ScriptBlock {
+  [Microsoft.PowerShell.PSConsoleReadline]::RevertLine()
+  [Microsoft.PowerShell.PSConsoleReadline]::Insert("cd $(ogh list -p | fzf)")
+  [Microsoft.PowerShell.PSConsoleReadline]::AcceptLine()
+}
+Set-PSReadlineKeyHandler -Key 'Ctrl+l' -ScriptBlock {
+  [Microsoft.PowerShell.PSConsoleReadline]::RevertLine()
+  [Microsoft.PowerShell.PSConsoleReadline]::AcceptLine()
+  Clear-Host
+}
+
+# Add 'open' command
+New-Alias open Invoke-Item
