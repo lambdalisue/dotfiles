bc3f7271e856a7b25127ede4ea86b3c39dcc668b
Alisue <lambdalisue@gmail.com>
2025-09-30 16:43:30 +0900

    Update

--- home/config/nvim/plugin.d/aibo.lua
+++ home/config/nvim/plugin.d/aibo.lua
@@ -1,12 +1,18 @@
-vim.api.nvim_create_user_command("Claude",
-  "Aibo -focus -opener='topleft vsplit' claude --permission-mode bypassPermissions", {})
-vim.api.nvim_create_user_command("Codex", "Aibo -focus -opener='topleft vsplit' codex", {})
-vim.api.nvim_create_user_command("Ollama", "Aibo -focus -opener='topleft vsplit' ollama run gpt-oss", {})
+local claude = "claude --permission-mode bypassPermissions"
+local opener = "rightbelow vsplit"
 
-vim.keymap.set("n", "<leader>ii", "<Cmd>Claude<CR>", { desc = "Chat with Claude" })
-vim.keymap.set("n", "<leader>ix", "<Cmd>Codex<CR>", { desc = "Chat with Codex" })
-vim.keymap.set("n", "<leader>io", "<Cmd>Ollama<CR>", { desc = "Chat with Ollama" })
+vim.keymap.set(
+  "n",
+  "<leader>ii",
+  string.format("<Cmd>Aibo -focus %s<CR>", claude),
+  { desc = "Chat with Claude" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>II",
+  string.format("<Cmd>Aibo -opener='%s' %s<CR>", opener, claude),
+  { desc = "Chat with Claude" }
+)
+
 vim.keymap.set("n", "<leader>is", "<Cmd>AiboSend -input<CR>", { desc = "Send to AI" })
 vim.keymap.set("v", "<leader>is", ":AiboSend -input<CR>", { desc = "Send to AI" })
-vim.keymap.set("v", "<leader>it", ':AiboSend -prefix="Translate the following to Japanese\\n\\n" -submit<CR>',
-  { desc = "Translate with AI" })
